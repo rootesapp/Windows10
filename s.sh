a27e35030a4b3b022b85e26df6d31dd045d92fde
rootesapp <htmlhtml5@foxmail.com>
2024-10-27 00:14:04 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,6 +1,9 @@
 sudo apt install unzip -y 
 sudo apt install python3-pip -y
 sudo unzip s.zip
-sudo python English.py
+sudo pip install googletrans==4.0.0-rc1
+
+
+sudo python English2.py
 sudo zip -r archive.zip *
 sudo curl -T archive.zip http://ik.gengye.top:5212/dav/玩机百宝箱Beta.apk --user 123456789@qq.com:ag6R6UI9cRuVPAK2rnqwUnmarlIYhD4M
