87a8aa2f37fee6e7613d017934ef3aa5281f67b1
rootesapp <htmlhtml5@foxmail.com>
2024-10-26 22:42:30 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 sudo apt install unzip -y 
 sudo apt install python3-pip -y
-unzip s.zip
-pip3 install requests
-python English.py
-zip -r archive.zip *
-curl -T archive.zip http://ik.gengye.top:5212/dav/玩机百宝箱Beta.apk --user 123456789@qq.com:ag6R6UI9cRuVPAK2rnqwUnmarlIYhD4M
+sudo unzip s.zip
+sudo pip3 install requests
+sudo python English.py
+sudo zip -r archive.zip *
+sudo curl -T archive.zip http://ik.gengye.top:5212/dav/玩机百宝箱Beta.apk --user 123456789@qq.com:ag6R6UI9cRuVPAK2rnqwUnmarlIYhD4M
